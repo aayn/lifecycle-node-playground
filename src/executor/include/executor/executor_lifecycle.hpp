e1744a49b93de2445175c2c6fbe2e3c30c5d6257
Aayush Naik <aayushnaik17@gmail.com>
2021-03-03 19:17:35 +0000

    Finish basic lifecyle example

--- src/executor/include/executor/executor_lifecycle.hpp
+++ src/executor/include/executor/executor_lifecycle.hpp
@@ -6,17 +6,18 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+#include "rclcpp/timer.hpp"
 #include "std_msgs/msg/string.hpp"
 
 namespace executor {
 
-class Executor : public rclcpp_lifecycle::LifecycleNode {
- public:
-  using LifecycleCbReturn =
+using LifecycleCbReturn =
       rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
+class Executor : public rclcpp_lifecycle::LifecycleNode {
+ public:
   Executor(const std::string& node_name, const std::string& ns,
-           rclcpp::NodeOptions options);
+           rclcpp::NodeOptions options=rclcpp::NodeOptions());
 
  protected:
   virtual LifecycleCbReturn on_configure(
@@ -36,12 +37,18 @@ class Executor : public rclcpp_lifecycle::LifecycleNode {
 
   virtual LifecycleCbReturn on_cleanup(
       const rclcpp_lifecycle::State& previous_state) override;
+    
+  virtual void publish_string();
+
+  virtual void publish_vel();
 
  private:
   rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr
       vel_publisher_;
   rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::String>::SharedPtr
       text_publisher_;
+  rclcpp::TimerBase::SharedPtr vel_timer_;
+  rclcpp::TimerBase::SharedPtr text_timer_;
 };
 
 }  // namespace executor
